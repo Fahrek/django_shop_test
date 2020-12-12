d8669fd4076571810853ed1e82e22860b9ab676b
Andres G. Quina <andresgqjob@gmail.com>
2020-12-12 14:30:45 +0100

    global var cntxt_processors

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
+from cart.cart import Cart
+from .models import Product
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Product
+
 
 
 # Create your views here.
 @login_required(login_url='/autenticacion/acceder')
 def listado_productos(request):
+    cart = Cart(request)
     products = Product.objects.all()
     return render(request, "products/listado.html", {"products": products})
